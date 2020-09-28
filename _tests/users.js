19769c6230198f321aed507b9d2fb4a2ea5a4a32
unknown <miloszz87@gmail.com>
2020-09-27 22:22:18 -0400

    Worked on tests

---
+++ _tests/users.js
@@ -0,0 +1,24 @@
+const supertest = require("supertest");
+const app = require("../server");
+
+describe("Users integration test", () => {
+  it("should register a user", async () => {
+    const res = await supertest(app).post("/api/v1/users/register").send({
+      username: "milooooo",
+      password: "123456",
+    });
+    expect(res.statusCode).toBe(201);
+    // .catch((err) => console.log(err));
+  });
+
+  let token;
+  it("Should login user", async () => {
+    const res = await supertest(app).post("/api/v1/users/login").send({
+      username: "milooooo",
+      password: "123456",
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token);
+    token = res.body.token;
+  });
+});
