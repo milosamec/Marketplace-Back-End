c568667ee2013eaed0adc73efa26c36c774e55d9
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-12 18:34:33 -0400

    Update User.js
    Comments

--- models/User.js
+++ models/User.js
@@ -1,22 +1,27 @@
 const db = require("../config/db");
 
+// Adds user in db and returns the id
 async function add(user) {
   const [id] = await db("users").insert(user);
   return findById(id);
 }
 
+// Finds user by username - id + username + pass
 function findBy(username) {
   return db("users").select("id", "username", "password").where(username);
 }
 
+// Finds user user by username and returns the id + username
 function findUserObject(username) {
   return db("users").select("id", "username");
 }
 
+// Finds user by id
 function findById(id) {
   return db("users").select("id", "username").where({ id }).first();
 }
 
+// Exports all defined functions
 module.exports = {
   findBy,
   add,
