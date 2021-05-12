d2b232d9782d9ba8d5d12f48270efb5a43773f75
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-12 18:47:03 -0400

    Update async.js
    Comments about async handler function

--- middleware/async.js
+++ middleware/async.js
@@ -1,4 +1,8 @@
+// Express doesn't support async await by default
+// This is a handy snippet of code enabling async await usage within express route handlers
+
 const asyncHandler = (fn) => (req, res, next) => {
+  // We use Promise.resolve to 
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 module.exports = asyncHandler;
