3dbeb7f12550a9ea24755b3e33ef054c87a23182
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-12 18:59:46 -0400

    Update db.js
    Comments

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
+// Import knex
 const knex = require("knex");
 
+// Grab config
 const knexConfig = require("../knexfile.js");
 
+// DB Connected
 console.log("Database Connected");
 
+// Export knex
 module.exports = knex(knexConfig.development);
