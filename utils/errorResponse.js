aff03c142d301bdc147e211386e903537eb60939
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-12 18:17:00 -0400

    Update errorResponse.js
    Comments

--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,5 +1,6 @@
 class ErrorResponse extends Error {
   constructor(message, statusCode) {
+    // Use super to access and call functions on an object's parent
     super(message);
     this.statusCode = statusCode;
   }
