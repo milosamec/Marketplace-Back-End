24ddfa37f5f5178e923c323efe2ff657ac80c343
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-12 18:32:18 -0400

    Update Product.js
    Comments

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,19 @@
 const db = require("../config/db");
 
+// Add function takes in product
 async function add(product) {
+  // grabs products id and adds it to the db
   const [id] = await db("products").insert(product);
+  // returns id of added product
   return findById(id);
 }
 
+// Returns all products
 async function findAll() {
   return await db("products");
 }
 
+// Finds product by it's id
 function findById(id) {
   return db("products")
     .select("id", "userid", "title", "description", "price", "image")
@@ -16,15 +21,19 @@ function findById(id) {
     .first();
 }
 
+// Updates product by taking in changes + products id
 async function update(changes, id) {
   await db("products").where({ id }).update(changes);
   return await findById(id);
 }
 
+
+// removes product by id
 async function remove(id) {
   await db("products").where({ id }).delete();
 }
 
+// Exports all defined functions
 module.exports = {
   add,
   findAll,
